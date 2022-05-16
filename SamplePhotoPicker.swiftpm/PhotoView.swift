ed0ec891c6a5a2b13a9990327d4d5590e0c0772e
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-05-16 14:39:26 +0900

    表示するPhotoの判定をifからswitch-caseに変更

--- SamplePhotoPicker.swiftpm/PhotoView.swift
+++ SamplePhotoPicker.swiftpm/PhotoView.swift
@@ -6,56 +6,60 @@ struct PhotoView: View {
   let provider: NSItemProvider
 
   @Binding var item: NSItemProviderReading?
-  @State var tappedImage: UIImage?
   @State var isPresentedVideoPlayer = false
   @State var didError = false
-  @State var error: PhotoViewError?
+  @State var error: PhotoError?
 
   var body: some View {
     GeometryReader { geometry in
       if let item = item {
-        if let livePhoto = item as? PHLivePhoto {
-          LivePhoto(livePhoto: livePhoto)
-        }
-        else if let uiImage = item as? UIImage {
-          Image(uiImage: uiImage)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-        } else if let url = item as? URL {
-          ZStack {
-            if let uiImage = try? UIImage(movieURL: url) {
-              Image(uiImage: uiImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            }
-
-            Image(systemName: "play")
-          }
-          .onTapGesture {
-            isPresentedVideoPlayer.toggle()
-          }
-          .sheet(isPresented: $isPresentedVideoPlayer) {
-            let player: AVPlayer = .init(url: url)
-            VideoPlayer(player: player)
-              .onAppear {
-                player.play()
+        switch item {
+          case let livePhoto as PHLivePhoto:
+            LivePhoto(livePhoto: livePhoto)
+          case let uiImage as UIImage:
+            Image(uiImage: uiImage)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+          case let movieURL as URL:
+            ZStack {
+              if let uiImage = try? UIImage(movieURL: movieURL) {
+                Image(uiImage: uiImage)
+                  .resizable()
+                  .aspectRatio(contentMode: .fit)
+                Image(systemName: "play")
+              } else {
+                Text("Failed Load Image")
               }
-          }
+            }
+            .onTapGesture {
+              isPresentedVideoPlayer = true
+            }
+            .sheet(isPresented: $isPresentedVideoPlayer) {
+              let player: AVPlayer = .init(url: movieURL)
+              VideoPlayer(player: player)
+                .onAppear {
+                  player.play()
+                }
+            }
+          default:
+            Text("Failed Load Error")
         }
       }
     }
-    //.alert(isPresented: $didError, error: $error, actions: { Text("") })
-
+    .alert(isPresented: $didError, error: error) {
+      Text("load error")
+    }
     .onAppear {
-      if item == nil {
-        Task {
-          do {
-            item = try await provider.loadPhoto()
-          } catch {
-            print("error")
-            self.error = .loadPhoto
-            self.didError = true
-          }
+      if item != nil {
+        return
+      }
+
+      Task {
+        do {
+          item = try await provider.loadPhoto()
+        } catch let newError {
+          self.error = newError as? PhotoError ?? .unknown
+          self.didError = true
         }
       }
     }
