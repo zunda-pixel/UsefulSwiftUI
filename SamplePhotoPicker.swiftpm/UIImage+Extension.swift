f3648d057fa9f2b1f516418bbda644ee46f6065a
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-05-16 16:34:56 +0900

    appliesPreferredTrackTransformをtrueにして画像を正しい方向で生成するように

--- SamplePhotoPicker.swiftpm/UIImage+Extension.swift
+++ SamplePhotoPicker.swiftpm/UIImage+Extension.swift
@@ -5,6 +5,7 @@ extension UIImage {
   public convenience init(movieURL url: URL) throws {
     let asset: AVAsset = .init(url: url)
     let generator = AVAssetImageGenerator(asset: asset)
+    generator.appliesPreferredTrackTransform = true
     let cgImage = try generator.copyCGImage(at: asset.duration, actualTime: nil)
     self.init(cgImage: cgImage)
   }
